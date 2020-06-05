8c8fac604baad21e98aa904216d95324f4b241a9
Alex Rodrigues <rodriguesalex793@gmail.com>
2020-06-05 18:57:04 -0300

    Added validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import multer from 'multer'
 import multerConfig from './config/multer'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 import ItemsController from './controllers/ItemsController'
 import PointsController from './controllers/PointsController'
@@ -14,7 +15,24 @@ const pointsController = new PointsController()
 routes.get('/items', itemsController.index)
 
 routes.get('/points', pointsController.index)
-routes.post('/points', upload.single('image'), pointsController.create)
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+)
 routes.get('/points/:id', pointsController.show)
 
 export default routes
